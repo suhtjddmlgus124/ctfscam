13382c6c8d0c10a35a1ebacc1cd61c89aa6719a0
suh124 <suhtjddmlgus124@gmail.com>
2025-05-14 12:48:28 +0900

    FEAT: 도메인 등록

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import fs from 'fs';
+
+const domain = "sasa2o25ctf.kro.kr";
+fs.writeFileSync('public/CNAME', domain);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ctfscam/',
+  base: '/',
 });
